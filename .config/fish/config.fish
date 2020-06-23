62c4e4862681cbed79e69eff9d56a596477e21fc
aswinmohanme <dev.aswinmohan@gmail.com>
2020-06-23 21:25:14 +0530

    Setup things for life

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,11 +5,11 @@ source ~/.profile;clear;
 # DISABLE TERMINAL COLORS
 functions -e set_color
 function set_color; end
- set -x RUBY_TESTOPTS '--color=never'
- set -x RUBYOPT '-W:no-deprecated'
+set -x RUBY_TESTOPTS '--color=never'
+set -x RUBYOPT '-W:no-deprecated'
 
-for color in (set | grep -Eo '^fish_color_[a-zA-Z0-9_]+')
+ for color in (set | grep -Eo '^fish_color_[a-zA-Z0-9_]+')
     # optional: save the color to be restored later
     #set -g old_$color $$color
-    set $color normal
+   set $color normal
 end
